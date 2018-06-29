c8d192526745bd27e417ea92c2c3aa1b0aed9d9b
Antonio Sanchez <antonysanchez02@gmail.com>
2018-06-29 18:06:30 -0400

    Primer activity

--- app/src/main/java/ado/com/itla/tartaro/MainActivity.java
+++ app/src/main/java/ado/com/itla/tartaro/MainActivity.java
@@ -1,8 +1,11 @@
 package ado.com.itla.tartaro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i(LOG_TAG, "Estoy Aqui");
+
+
+        Button btnCategoria = (Button) findViewById(R.id.btnCategoria); //busca todos los botones que no esten en el MainActivity
+
+        btnCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: llamar otro Activity
+
+                Intent VistaCategoria = new Intent(MainActivity.this, VistaCategoria.class); //Llama la vista creada de categoria
+
+                VistaCategoria.putExtra("nombre", "Juan de lo palote");
+                startActivity(VistaCategoria); //inicializa el activity
+
+
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/ado/com/itla/tartaro/VistaCategoria.java
@@ -0,0 +1,22 @@
+package ado.com.itla.tartaro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class VistaCategoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_categoria);
+
+        TextView txtNombre = (TextView) findViewById(R.id.txtNombre);
+
+        Bundle data = this.getIntent().getExtras();
+        String nombre = (String) data.getString("nombre");
+        txtNombre.setText(nombre);
+
+
+    }
+}
